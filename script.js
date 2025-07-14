5eb28415d4488399afcc5b54b1b809b002254207
ColorblindGuy <110788949+ColorblindGuy@users.noreply.github.com>
2025-07-14 18:02:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 // --- Firebase Configuration & Initialization ---
 // =================================================================
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
